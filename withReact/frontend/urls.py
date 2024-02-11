72e7c55535d2feae43108216c440e704950e0dc0
kouki928 <furukawavo@gmail.com>
2024-02-11 23:57:15 +0900

    403Error

--- withReact/frontend/urls.py
+++ withReact/frontend/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index ),
+    # admin page ------------------------------------------------------------ #
+    path("staff/", views.site_management, name="topPage"),
+    
+    # client page ----------------------------------------------------------- #
+    path('', views.index, name="index" ),
+    
+    # error page ------------------------------------------------------------ #
+    path("403/", views.Error_403, name="403"),
 ]
--- withReact/frontend/views.py
+++ withReact/frontend/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 def index(request):
     return render(request, 'frontend/index.html')
+
+def site_management(request):
+    if not request.user.is_superuser:
+        return redirect('403')
+        
+    return render(request, 'staff/index.html')
+
+
+def Error_403(request):
+    return render(request, 'errors/403.html')
